91f950b169a9ea5f539a95fbabc1cfd6351ebf3d
IetermagogSA <39064341+IetermagogSA@users.noreply.github.com>
2018-12-14 09:16:55 +0200

    Including the breaking blocks project
    Breaking blocks is an Arkanoid-type game with a Breaking Bad theme

---
+++ BreakingBlocks/BreakingBlocks/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+
+	[SerializeField] float minX = 1f;
+	[SerializeField] float maxX = 15f;
+	[SerializeField] float screenWidthInUnits = 16f;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+		float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+		Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+
+		paddlePos.x = Mathf.Clamp(mousePosInUnits, 0f, 16f);
+
+		transform.position = paddlePos;
+	}
+}
