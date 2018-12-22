4e754714e7be14dbb5f67a84f7ed96b6e23ce831
IetermagogSA <39064341+IetermagogSA@users.noreply.github.com>
2018-12-22 13:49:23 +0200

    Added another enemy, flightpaths and enemy spawner

---
+++ Hawks of the Frontline/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    // Config Params
+    WaveConfig waveConfig;
+
+    // State variables
+    List<GameObject> paths;
+    List<Transform> pathWayPointList;
+    int currentWayPointIndex = 0;
+
+    private void Start()
+    {
+        GameObject path = waveConfig.GetPath();
+        pathWayPointList = path.GetComponentsInChildren<Transform>().ToList();
+        // Remove the parent object
+        pathWayPointList.RemoveAt(0);
+
+        transform.position = pathWayPointList[currentWayPointIndex].transform.position;
+    }
+
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (currentWayPointIndex <= pathWayPointList.Count - 1)
+        {
+            var targetPosition = pathWayPointList[currentWayPointIndex].transform.position;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                currentWayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Hawks of the Frontline/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnAllWaves());
+    }
+
+    IEnumerator SpawnAllEnemiesInWave(WaveConfig currentWaveConfig)
+    {
+        currentWaveConfig.GenerateRandomgPath();
+
+        for (int enemyCounter = 1; enemyCounter <= currentWaveConfig.GetNumberOfEnemies(); enemyCounter++)
+        {
+            var newEnemy = Instantiate(currentWaveConfig.GetEnemyPrefab(), currentWaveConfig.GetPath().transform.position, new Quaternion(180f,0f,0f,0f));
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(currentWaveConfig);
+            yield return new WaitForSeconds(currentWaveConfig.GetTimeBetweenSpawns());
+        }
+    }
+
+    IEnumerator SpawnAllWaves()
+    {
+        foreach(WaveConfig waveConfig in waveConfigs)
+        {
+            yield return StartCoroutine(SpawnAllEnemiesInWave(waveConfig));
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
