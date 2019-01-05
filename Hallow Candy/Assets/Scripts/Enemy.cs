8565c73498932d01626d6f9298dfdebce40a3607
IetermagogSA <39064341+IetermagogSA@users.noreply.github.com>
2019-01-05 17:33:17 +0200

    Added enemy and defender
    Added an enemy and a spawner for the enemies
    Added the first defender

---
+++ Hallow Candy/Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Range(0f,10f)]
+    [SerializeField] float speed = 0.25f;
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+    }
+
+    public void SetMovementSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
---
+++ Hallow Candy/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemiesToSpawn;
+    [SerializeField] float minSpawnRate = 10f;
+    [SerializeField] float maxSpawnRate = 20f;
+
+    bool enableSpawn = true;
+
+    IEnumerator Start()
+    {
+        while(enableSpawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnRate, maxSpawnRate));
+            SpawnEnemy();
+        }
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject.Instantiate(enemiesToSpawn, transform.position, Quaternion.identity);
+    }
+
+}
