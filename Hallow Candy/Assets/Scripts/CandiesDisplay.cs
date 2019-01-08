a2aa8fbde9d45364f9cd757e0c053ce396744780
Stefan du Toit <shdutoit@yahoo.com>
2019-01-08 10:07:56 +0200

    Added resource manager on levels
    Added a resource manager that will enable us to "pay" for defenders

---
+++ Hallow Candy/Assets/Scripts/CandiesDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CandiesDisplay : MonoBehaviour
+{
+    [SerializeField] int candies = 100;
+
+    TextMeshProUGUI candiesDisplayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        candiesDisplayText = GetComponent<TextMeshProUGUI>();
+        UpdateCandiesDisplay();
+    }
+
+    public void UpdateCandiesDisplay()
+    {
+        candiesDisplayText.text = candies.ToString();
+    }
+
+    public void AddCandies(int amountToAdd)
+    {
+        candies += amountToAdd;
+        UpdateCandiesDisplay();
+    }
+
+    public void DeceaseCandies(int amountToDecrease)
+    {
+        if (amountToDecrease <= candies)
+        {
+            candies -= amountToDecrease;
+            UpdateCandiesDisplay();
+        }
+    }
+}
--- Hallow Candy/Assets/Scripts/DefenderButton.cs
+++ Hallow Candy/Assets/Scripts/DefenderButton.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class DefenderButton : MonoBehaviour
 {
+    [SerializeField] Defender defender;
+
     SpriteRenderer sprite;
+
+    private void OnMouseDown()
+    {
+        FindObjectOfType<DefenderSpawner>().SetSelectedDefender(defender);
+    }
     private void OnMouseOver()
     {
         sprite = GetComponent<SpriteRenderer>();
