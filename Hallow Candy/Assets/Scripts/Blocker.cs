6a2f75f649925298dddeb8c285ff46f2570f0568
Stefan du Toit <39064341+IetermagogSA@users.noreply.github.com>
2019-01-17 15:55:42 +0200

    Adding some files I forgot to add with previous commit

---
+++ Hallow Candy/Assets/Scripts/Blocker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocker : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            animator.SetBool("isBlocking", true);
+        }
+    }
+}
