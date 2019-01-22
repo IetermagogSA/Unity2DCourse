961fff3511be96000ee7b1483c700d8cd2f04821
Stefan du Toit <39064341+IetermagogSA@users.noreply.github.com>
2019-01-22 12:52:05 +0200

    Committing to include new files

---
+++ Hallow Candy/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour
+{
+    [Tooltip("The total time of the level in SECONDS")]
+    [SerializeField] float levelTimeInSeconds = 60;
+    void Update()
+    {
+        GetComponent<Slider>().value = (Time.timeSinceLevelLoad / levelTimeInSeconds) * 100;
+
+        bool levelTimerFinished = (Time.timeSinceLevelLoad > levelTimeInSeconds);
+
+        if(levelTimerFinished)
+        {
+            Debug.Log("The level is complete!");
+        }
+    }
+}
